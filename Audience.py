ebf0c4bdbb74a4643fd0141eac64a586b4409719
abellettre <abellettre@neocisinc.com>
2018-05-12 22:24:58 +0200

    Add an Audience class

---
+++ Audience.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy
+import copy
+
+from PersonSensor import *
+
+
+class Audience:
+    """Class to handle the audience"""
+    def __init__(self, personSensor):
+        self.persons = []
+        self.personSensor = personSensor
+
+    def update(self):
+        self.persons = self.personSensor.getPersons(self.persons)
+
+
+
+if __name__ == '__main__':
+    # Tests
+    personSensor = PersonSensor([Camera(0)])
+    personSensor.show = True
+
+    audience = Audience(personSensor)
+
+    while True:
+        audience.update()
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
