694fb7b297328277faf8e54c83ef81a1695679c2
abellettre <abellettre@neocisinc.com>
2018-05-12 22:34:49 +0200

    Create the Audience object in the main application and link it with the PersonSensor.

--- run.py
+++ run.py
@@ -1,18 +1,34 @@
 import sys
+
 from SensorModule import SensorModule
 from EmotionModule import EmotionModule
 from ResponseModule import ResponseModule
 from ActionModule import ActionModule
+from Audience import Audience
+
+show = True
+if show:
+	import cv2
 
 def runCatsCradle(config):
 	"""Main entry point for Cat's Cradle."""
+	global show
 	action_module = ActionModule(config)
 	response_module = ResponseModule(config,action_module)
 	emotion_module = EmotionModule(config,response_module)
 	sensor_module = SensorModule(config,emotion_module)
 
+	audience = Audience(sensor_module.personSensor)
+	audience.personSensor.show = show
+
 	while True:
-		pass
+		audience.update()
+		#print "found ", len(audience.persons), " persons"
+
+		if show:
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+
 
 def initialise():
 	config = {}
@@ -20,5 +36,10 @@ def initialise():
 
 if __name__ == "__main__":
 	config = initialise()
+
 	runCatsCradle(config)
+
+	if show:
+		cv2.destroyAllWindows()
+
 	sys.exit()
