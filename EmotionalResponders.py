b7cc1f2875104e02d14b96d7181331e9246a5cfe
mahzoon <mmahzoon@uncc.edu>
2019-02-19 21:02:19 -0500

    adding rules to change emotions. The complete rules are in a spreadsheet

--- EmotionalResponders.py
+++ EmotionalResponders.py
@@ -64,27 +64,46 @@ class EntryResponder(Responder):
 
     def respond(self, emotion_module, audience, idle):
         emotional_effect = {}
-        for person in audience.persons:
-            if person not in audience.previousPersons:
-                print "entry!"
-                if person.ageRange == "child":
-                    try_add(emotional_effect, "longing", EMOTION_DELTAS["large"])
-                    person.labels.add("Want")
-                    if random() < self.p * 1.5:
-                        self.response_module.lookAt(person, duration=0.5)
-                else:
-                    if random() < self.p:
-                        self.response_module.glanceAt(person, duration=0.1)
-                if audience.numNewRecently() > 3:
-                    print "lots of entries!"
-                    try_add(emotional_effect, "shame", EMOTION_DELTAS["moderate"])
+        
+        # rule 1.1
+        if audience.get_num_people_with_condition(None, 'adult', 'F', 'new') > 2:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["large"])
+            print("rule 1.1")
+        # rule 1.2
+        elif audience.get_num_people_with_condition(None, 'adult', 'F', 'new') > 0:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["medium"])
+            print("rule 1.2")
+        # rule 1.3
+        elif audience.get_num_people_with_condition(None, 'senior', None, 'new') > 0:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["small"])
+            print("rule 1.3")
+        
+        # rule 1.4
+        if audience.get_num_people_with_condition(None, 'child', None, 'new') > 0 and audience.get_num_people_with_condition(None, 'adult', None, 'new') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["large"])
+            print("rule 1.4")
+        # rule 1.5
+        elif audience.get_num_people_with_condition(None, 'child', None, 'new') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["large"])
+            print("rule 1.5")
+        # rule 1.6
+        elif audience.get_num_people_with_condition(None, 'adult', 'F', 'new') > 0 and audience.get_num_people_with_condition(None, 'adult', 'M', 'new') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["medium"])
+            print("rule 1.6")
+
+        # glance / look rule
+        # ??
+        # self.response_module.lookAt(person, duration=0.5)
+        # self.response_module.glanceAt(person, duration=0.1)
+
         if len(emotional_effect):
             emotion_module.affectEmotions(emotional_effect)
 
 
 #Responds to people who walk towards her. Variety of effects.
 class ApproachResponder(Responder):
-    def __init__(self, action_module, response_module, p=0.5, slow_size_ratio_range = [1.2,1.3], approach_size_ratio = 1.5, threat_size_ratio = 1.75):
+    def __init__(self, action_module, response_module, p=0.5, slow_size_ratio_range = [1.2,1.3],
+                 approach_size_ratio = 1.5, threat_size_ratio = 1.75):
         Responder.__init__(self,action_module, response_module, p)
         self.approach_size_ratio = approach_size_ratio
         self.threat_size_ratio = threat_size_ratio
@@ -92,6 +111,8 @@ class ApproachResponder(Responder):
 
     def respond(self, emotion_module, audience, idle):
         emotional_effect = {}
+        
+        # adding tags based on proximity and speed of audience
         for person in audience.persons:
             if person.faceSizeHistory is None or len(person.faceSizeHistory) == 0 or person.faceSizeHistory[0] is None:
                 continue
@@ -105,46 +126,90 @@ class ApproachResponder(Responder):
                     person.interestingness += 5
                 else:
                     person.labels.discard("RecentApproach")
-            # If any of them are approaching fast, add fear (more for men, less for children and seniors), interest and probably glance + respond
+            # If any of them are approaching fast, add "threat" tag
             if person.faceSizeHistory[0] / min(person.faceSizeHistory) > self.threat_size_ratio:
                 if not "RecentThreat" in person.labels:
                     person.labels.add("Threat")
                     person.labels.add("RecentThreat")
                     person.interestingness += 25
-                    if person.ageRange == "child":
-                        try_add(emotional_effect, "surprise", EMOTION_DELTAS["extreme"])
-                        self.response_module.lookAt(person, duration=0.5)
-                    elif person.ageRange == "senior" or person.gender == "F":
-                        try_add(emotional_effect, "fear", EMOTION_DELTAS["extreme"])
-                        if random() < self.p*1.5:
-                            self.response_module.lookAt(person, duration=0.25)
-                    elif person.gender == "M":
-                        try_add(emotional_effect, "fear", EMOTION_DELTAS["instant"])
-                        person.interestingness += 25
-                        self.response_module.lookAt(person, duration=0.5)
-            else:
-                person.labels.discard("RecentThreat")
-            #If any of them are approaching slow, add fear if male, shame if woman or senior, and longing if child, then probably glance
+                else:
+                    person.labels.discard("RecentThreat")
+            # If any of them are approaching slow, add "creeping" tag
             max_size_diff = max(person.faceSizeHistory) / min(person.faceSizeHistory)
             if max_size_diff > self.slow_size_ratio_range[0] and max_size_diff < self.slow_size_ratio_range[1]:
                 if not "RecentCreeping" in person.labels:
                     person.labels.add("Creeping")
                     person.labels.add("RecentCreeping")
                     person.interestingness += 5
-                    if person.ageRange == "child":
-                        try_add(emotional_effect, "longing", EMOTION_DELTAS["extreme"])
-                        person.labels.add("Want")
-                        self.response_module.glanceAt(person, duration=0.25)
-                    elif person.ageRange == "senior" or person.gender == "F":
-                        try_add(emotional_effect, "shame", EMOTION_DELTAS["large"])
-                        if random() < self.p:
-                            self.response_module.glanceAt(person, duration=0.25)
-                    elif person.gender == "M":
-                        try_add(emotional_effect, "fear", EMOTION_DELTAS["large"])
-                        if random() < self.p:
-                            self.response_module.glanceAt(person, duration=0.25)
                 else:
                     person.labels.discard("RecentCreeping")
+            
+        # respond based on tags
+        
+        # rule 2.1
+        if audience.get_num_people_with_condition('Approached', 'adult', 'M', 'current') > 0:
+            try_add(emotional_effect, "fear", EMOTION_DELTAS["extreme"])
+            print("rule 2.1")
+        # rule 2.2
+        elif audience.get_num_people_with_condition('Threat', 'adult', 'F', 'current') > 0:
+            try_add(emotional_effect, "fear", EMOTION_DELTAS["large"])
+            print("rule 2.2")
+        # rule 2.3
+        elif audience.get_num_people_with_condition('Threat', 'senior', None, 'current') > 0:
+            try_add(emotional_effect, "fear", EMOTION_DELTAS["large"])
+            print("rule 2.3")
+        
+        # rule 2.4
+        if audience.get_num_people_with_condition('Approached', 'adult', 'F', 'current') > 0:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["medium"])
+            print("rule 2.4")
+        # rule 2.5
+        elif audience.get_num_people_with_condition('Creeping', 'adult', 'F', 'current') > 2:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["large"])
+            print("rule 2.5")
+        # rule 2.6
+        elif audience.get_num_people_with_condition('Creeping', 'senior', None, 'current') > 0:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["small"])
+            print("rule 2.6")
+        # rule 2.7
+        elif audience.get_num_people_with_condition('Creeping', None, None, 'current') > 5:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["large"])
+            print("rule 2.7")
+        
+        # rule 2.8
+        if audience.get_num_people_with_condition('Threat', 'adult', 'M', 'current') > 0:
+            try_add(emotional_effect, "surprise", EMOTION_DELTAS["instant"])
+            print("rule 2.8")
+        # rule 2.9
+        elif audience.get_num_people_with_condition('Threat', 'child', None, 'current') > 0:
+            try_add(emotional_effect, "surprise", EMOTION_DELTAS["instant"])
+            print("rule 2.9")
+
+        # rule 2.10
+        if audience.get_num_people_with_condition('Creeping', 'adult', 'F', 'current') > 0 and audience.get_num_people_with_condition('Creeping', 'adult', 'M', 'current') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["medium"])
+            print("rule 2.10")
+        # rule 2.11
+        elif audience.get_num_people_with_condition('Approached', 'adult', None, 'current') > 0 and audience.get_num_people_with_condition('Approached', 'child', None, 'current') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["large"])
+            print("rule 2.11")
+        # rule 2.12
+        elif audience.get_num_people_with_condition('Creeping', 'child', None, 'current') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["large"])
+            print("rule 2.12")
+        # rule 2.13
+        elif audience.get_num_people_with_condition('Creeping', 'senior', None, 'current') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["small"])
+            print("rule 2.13")
+        # rule 2.14
+        elif audience.get_num_people_with_condition('Approached', 'adult', 'F', 'current') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["medium"])
+            print("rule 2.14")
+        # rule 2.15
+        elif audience.get_num_people_with_condition('Creeping', 'adult', 'F', 'current') > 2:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["medium"])
+            print("rule 2.15")
+
         if len(emotional_effect):
             emotion_module.affectEmotions(emotional_effect)
 
@@ -162,16 +227,44 @@ class DepartResponder(Responder):
                 if "Approached" in person.labels:
                     if person.interestingness < HIGH_INTEREST_THRESH:
                         person.interestingness += 5
-                        if person.ageRange == "child":
-                            try_add(emotional_effect, "longing", EMOTION_DELTAS["large"])
                     else:
                         person.interestingness += 10
-                        if person.ageRange == "child":
-                            try_add(emotional_effect,"longing", EMOTION_DELTAS["extreme"])
                     if audience.numLostRecently() > 3:
                         if random() < self.p:
                             self.response_module.lookAt(person, duration=1)
 
+        # respond
+        
+        # rule 3.1
+        if audience.get_num_people_with_condition(None, 'child', None, 'lost') > 0:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["large"])
+            print("rule 3.1")
+        # rule 3.2
+        elif audience.get_num_people_with_condition(None, 'adult', 'F', 'lost') > 0 and audience.get_num_people_with_condition(None, 'adult', 'M', 'lost') > 0:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["large"])
+            print("rule 3.2")
+        # rule 3.3
+        elif audience.get_num_people_with_condition(None, 'senior', None, 'lost') > 0:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["medium"])
+            print("rule 3.3")
+        
+        # rule 3.4
+        if audience.get_num_people_with_condition(None, 'adult', 'F', 'lost') > 2:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["small"])
+            print("rule 3.4")
+        # rule 3.5
+        elif audience.get_num_people_with_condition(None, 'senior', None, 'lost') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["small"])
+            print("rule 3.5")
+        # rule 3.6
+        elif audience.get_num_people_with_condition(None, 'adult', None, 'lost') > 0 and audience.get_num_people_with_condition(None, 'child', None, 'lost') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["medium"])
+            print("rule 3.6")
+        # rule 3.7
+        elif audience.get_num_people_with_condition(None, 'adult', 'F', 'lost') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["medium"])
+            print("rule 3.7")
+        
         if len(emotional_effect):
             emotion_module.affectEmotions(emotional_effect)
 
@@ -191,17 +284,31 @@ class TooCloseResponder(Responder):
         #Check to see if anyone is standing too close
         for person in audience.persons:
             if person.faceSize() > too_close_size:
-                person.interestingness += 5
-                if person.ageRange == "child":
-                    try_add(emotional_effect, "fear", EMOTION_DELTAS["tiny"])
-                elif person.ageRange == "senior":
-                    try_add(emotional_effect, "shame", EMOTION_DELTAS["small"])
-                elif person.gender == "F":
-                    try_add(emotional_effect, "fear", EMOTION_DELTAS["small"])
-                elif person.gender == "M":
-                    try_add(emotional_effect, "fear", EMOTION_DELTAS["moderate"])
+                if not "RecentClose" in person.labels:
+                    person.labels.add("Close")
+                    person.labels.add("RecentClose")
+                    person.interestingness += 5
+                else:
+                    person.labels.discard("RecentClose")
                 if random() < self.p:
                     self.response_module.glanceAt(person, duration=0.1)
+    
+        # respond
+        
+        # rule 4.1
+        if audience.get_num_people_with_condition('Close', 'adult', 'M', 'current') > 0:
+            try_add(emotional_effect, "fear", EMOTION_DELTAS["extreme"])
+            print("rule 4.1")
+        
+        # rule 4.2
+        if audience.get_num_people_with_condition('Close', 'adult', 'F', 'current') > 0:
+            try_add(emotional_effect, "surprise", EMOTION_DELTAS["instant"])
+            print("rule 4.2")
+        # rule 4.3
+        elif audience.get_num_people_with_condition('Close', 'senior', None, 'current') > 0:
+            try_add(emotional_effect, "surprise", EMOTION_DELTAS["instant"])
+            print("rule 4.3")
+        
         if len(emotional_effect):
             emotion_module.affectEmotions(emotional_effect)
             
@@ -226,21 +333,77 @@ class MovementResponder(Responder):
                     person.interestingness += 5
                     if random() < self.p:
                         self.response_module.glanceAt(person, duration=0.5)
-                    if person.ageRange is "child":
-                        try_add(emotional_effect, "longing", EMOTION_DELTAS["large"])
-                    elif person.ageRange is "adult" and person.gender == "M":
-                        try_add(emotional_effect, "fear", EMOTION_DELTAS["moderate"])
-                    else:
-                        try_add(emotional_effect, "shame", EMOTION_DELTAS["moderate"])
                 adjacentdists = np.diagonal(pairdists,offset=1)
                 if np.all(adjacentdists<MAX_STILLNESS_MOVEMENT):
                     person.labels.add("Still")
                     person.interestingness += 2
-                    if person.ageRange is not "child":
-                        if person.ageRange is "adult" and person.gender == "M":
-                            try_add(emotional_effect, "fear", EMOTION_DELTAS["moderate"])
-                        else:
-                            try_add(emotional_effect, "shame", EMOTION_DELTAS["moderate"])
+    
+        # respond
+        
+        # rule 5.1
+        if audience.get_num_people_with_condition('Moving', None, None, 'current') > 2:
+            try_add(emotional_effect, "fear", EMOTION_DELTAS["large"])
+            print("rule 5.1")
+        # rule 5.2
+        elif audience.get_num_people_with_condition('Moving', 'adult', 'M', 'current') > 0:
+            try_add(emotional_effect, "fear", EMOTION_DELTAS["extreme"])
+            print("rule 5.2")
+        
+        # rule 5.3
+        if audience.get_num_people_with_condition('Moving', 'adult', 'F', 'current') > 0:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["medium"])
+            print("rule 5.3")
+        # rule 5.4
+        elif audience.get_num_people_with_condition('Moving', None, None, 'current') > 5:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["large"])
+            print("rule 5.4")
+        # rule 5.5
+        elif audience.get_num_people_with_condition('Moving', 'senior', None, 'current') > 0:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["small"])
+            print("rule 5.5")
+        # rule 5.6
+        elif audience.get_num_people_with_condition('Still', 'adult', 'F', 'current') > 2:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["small"])
+            print("rule 5.6")
+        # rule 5.7
+        elif audience.get_num_people_with_condition('Still', 'senior', None, 'current') > 0:
+            try_add(emotional_effect, "shame", EMOTION_DELTAS["small"])
+            print("rule 5.7")
+
+#        # rule 5.8
+#        if audience.get_num_people_with_condition('Moving', 'adult', 'M', 'current') > 0:
+#            try_add(emotional_effect, "surprise", EMOTION_DELTAS["instant"])
+#            print("rule 5.8")
+#        # rule 5.9
+#        elif audience.get_num_people_with_condition('Moving', 'adult', 'F', 'current') > 0:
+#            try_add(emotional_effect, "surprise", EMOTION_DELTAS["instant"])
+#            print("rule 5.9")
+#        # rule 5.10
+#        elif audience.get_num_people_with_condition('Moving', 'child', None, 'current') > 0:
+#            try_add(emotional_effect, "surprise", EMOTION_DELTAS["instant"])
+#            print("rule 5.10")
+
+        # rule 5.8
+        if audience.get_num_people_with_condition('Moving', 'child', None, 'current') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["large"])
+            print("rule 5.8")
+        # rule 5.9
+        elif audience.get_num_people_with_condition('Moving', 'adult', 'F', 'current') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["medium"])
+            print("rule 5.9")
+        # rule 5.10
+        elif audience.get_num_people_with_condition('Moving', 'adult', 'F', 'current') > 0 and audience.get_num_people_with_condition('Moving', 'adult', 'M', 'current') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["medium"])
+            print("rule 5.10")
+        # rule 5.11
+        elif audience.get_num_people_with_condition('Still', 'adult', 'F', 'current') > 2:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["small"])
+            print("rule 5.11")
+        # rule 5.12
+        elif audience.get_num_people_with_condition('Still', 'adult', None, 'current') > 0 and audience.get_num_people_with_condition('Still', 'child', None, 'current') > 0:
+            try_add(emotional_effect, "longing", EMOTION_DELTAS["small"])
+            print("rule 5.12")
+
         if len(emotional_effect):
             emotion_module.affectEmotions(emotional_effect)
             
